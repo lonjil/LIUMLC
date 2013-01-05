5dcf887a4470d571d9af72a1939455582c885b99
Birk Hirdman <lonjil@gmail.com>
2013-01-05 15:42:44 +0100

    Added functions ...
    draw-map, register-entity, player-gen and get-coord.

--- rogue.lisp
+++ rogue.lisp
@@ -1,9 +1,9 @@
-(defparameter *hight* 10)
+(defparameter *height* 10)
 (defparameter *width* 10)
 (defparameter *entities* nil)
 (setq *world* (progn
 		(let ((world nil))
-		  (loop for x to *hight*
+		  (loop for x to *height*
 			do (loop for y to *width*
 				 do (setq world (acons (list x y) 'floor world))))
 		  world)))
@@ -23,17 +23,22 @@
   (easter-egg -10)
   (magic 0))
 (defun register-entity (entity &rest keywords)
-  (setf *entities* (acons entity ((lambda (keys) make-entity keys `,@keywords) *entities*)))
+  (setf *entities* (acons entity (apply #'make-entity keywords) *entities*)))
 (defun get-entity-val (entity val)
-  (funcall val (cdr (assoc *entities* entity))))
-(defun player-gen (&optional (name "Casimir")) (make-entity :hp 20 :magic 16 :type 'player :x (random *hight*) :y (random *width*) :name name))
+  (funcall (intern (concatenate 'string "ENTITY-" (string val))) (cdr (assoc entity *entities*))))
+(defun entity-gen () nil)
+(defun player-gen (&key (name "Casimir") (hp 20) (magic 16) (type 'player) (y (random *height*)) (x (random *height*))) (make-entity :hp hp :magic magic :type type :x x :y y :name name))
 (defun create-entites ()
   (setf player (player-gen))
   (setf mob (make-entity)))
 (defun draw-map (entities world)
   (loop for x in *hight*
 	do (loop for y in *width*
-		 do (print (cond (()))))))
+		 do (print (cond ((equal `(,x . ,y) (get-coord 'player)) #\@)
+				 ((equal `(,x . ,y) (get-coord 'mob)) #\M)
+				 (t #\.))))))
+(defun get-coord (entity)
+  `(,(entity-x (cdr (assoc entity *entities*))) . ,(entity-y (cdr (assoc 'player *entities*)))))
 (defun set-coord (entity coord)
   (setf (entity-x entity) (car coord))
   (setf (entity-y entity) (cdr coord)))
